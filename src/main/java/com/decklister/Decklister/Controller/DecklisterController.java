24a7d952286444f4a78628c64369bf3b4a58ee71
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-02 11:33:35 +0200

    Authenifizierung 3. Versuch

--- src/main/java/com/decklister/Decklister/Controller/DecklisterController.java
+++ src/main/java/com/decklister/Decklister/Controller/DecklisterController.java
@@ -2,10 +2,18 @@ package com.decklister.Decklister.Controller;
 
 import com.decklister.Decklister.persistence.model.Card;
 import com.decklister.Decklister.persistence.model.Deck;
+import com.decklister.Decklister.persistence.repository.CardRepository;
 import com.decklister.Decklister.persistence.repository.DeckRepository;
 import com.decklister.Decklister.service.DecklisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(value = "/decklister")
@@ -16,11 +24,13 @@ public class DecklisterController {
     @Autowired
     private DeckRepository deckRepository;
 
+    @Secured("ROLE_JUDGE")
     @GetMapping("/decks")
     public Iterable<Deck> findAllDecks() {
         return decklisterService.findAllDecks();
     }
 
+    @Secured({"ROLE_PARTICIPANT","ROLE_JUDGE"})
     @PostMapping(value = "/decks")
     public Deck createDeck(@RequestBody Deck newDeck) {
         return decklisterService.createDeck(newDeck);
