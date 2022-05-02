2d29918fad913044202849f9ff24363316fe2862
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-02 10:39:38 +0200

    persistence mysql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO Deck (id,name,uuid) VALUES (1,'SneakAndShow',uuid());
-INSERT INTO Deck (id,name,uuid) VALUES (2,'Elves',uuid());
+INSERT INTO deck (id,name,uuid) VALUES (1,'SneakAndShow',uuid());
+INSERT INTO deck (id,name,uuid) VALUES (2,'Elves',uuid());
 
-INSERT INTO Card (id,name, quantity, uuid,deck_id) VALUES (1,'Griselbrand', 4, uuid(),1);
-INSERT INTO Card (id,name, quantity, uuid,deck_id) VALUES (2,'Sneak Attack', 4, uuid(),1);
-INSERT INTO Card (id,name, quantity, uuid,deck_id) VALUES (3,'Forest', 7, uuid(), 2);
-INSERT INTO Card (id,name, quantity, uuid,deck_id) VALUES (4,'Nettle Sentinel', 4, uuid(),2);
+INSERT INTO card (id,name, quantity, uuid,deck_id) VALUES (1,'Griselbrand', 4, uuid(),1);
+INSERT INTO card (id,name, quantity, uuid,deck_id) VALUES (2,'Sneak Attack', 4, uuid(),1);
+INSERT INTO card (id,name, quantity, uuid,deck_id) VALUES (3,'Forest', 7, uuid(), 2);
+INSERT INTO card (id,name, quantity, uuid,deck_id) VALUES (4,'Nettle Sentinel', 4, uuid(),2);
