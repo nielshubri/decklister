b90671fc7dcd4f3e136595baadf704dd482ee98c
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-03 11:56:37 +0200

    dev und prod profile

--- src/main/java/com/decklister/Decklister/config/SecurityConfigProd.java
+++ src/main/java/com/decklister/Decklister/config/SecurityConfigProd.java
@@ -12,13 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@Profile("prod")
 public class SecurityConfigProd extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("judge").password(passwordEncoder().encode("123")).roles("JUDGE").and().withUser(
-                "participant").password(passwordEncoder().encode("123")).roles("PARTICIPANT");
+        auth.inMemoryAuthentication().withUser("judge").password(passwordEncoder().encode("123")).roles("JUDGE").and().withUser("participant").password(passwordEncoder().encode("123")).roles("PARTICIPANT");
     }
 
     @Bean
@@ -28,12 +26,7 @@ public class SecurityConfigProd extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 
         http.csrf().disable();
 
