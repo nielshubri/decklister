9e3fcbe1860bb8e7edff4bcba4d31f3d6a07195e
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-03 11:57:12 +0200

    dev und prod profile

--- src/main/java/com/decklister/Decklister/config/SecurityConfig.java
+++ src/main/java/com/decklister/Decklister/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.decklister.Decklister.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigProd extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
