e07ca48ddc823a3287475003f0d6848cfc35d8b3
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-09 15:52:18 +0200

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,3 @@ INSERT INTO card (id, uuid, name, quantity, deck_id) VALUES (1, uuid(), 'Griselb
 INSERT INTO card (id, uuid, name, quantity, deck_id) VALUES (2, uuid(), 'Sneak Attack', 4, 1);
 INSERT INTO card (id, uuid, name, quantity, deck_id) VALUES (3, uuid(), 'Forest', 7, 2);
 INSERT INTO card (id, uuid, name, quantity, deck_id) VALUES (4, uuid(), 'Nettle Sentinel', 4, 2);
-
-INSERT INTO user (id, uuid, email, password, role) VALUES (1, uuid(), 'niels.huntebrinker@posteo.de', 'pass', 'test');
-INSERT INTO user (id, uuid, email, password, role) VALUES (2, uuid(), 'judge', '123', 'ROLE_JUDGE');
