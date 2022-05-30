e12efecf94d09fd25ee08881932131f070b9ca96
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-05-30 16:53:08 +0200

    .

--- src/main/java/com/decklister/Decklister/Controller/DecklisterController.java
+++ src/main/java/com/decklister/Decklister/Controller/DecklisterController.java
@@ -6,6 +6,7 @@ import com.decklister.Decklister.service.DecklisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class DecklisterController {
     @Autowired
