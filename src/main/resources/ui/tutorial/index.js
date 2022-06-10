12fb99b6ae717f89b313927fec7b019e3be16861
Niels Huntebrinker <n.huntebrinker@alphaquest.de>
2022-06-10 14:03:33 +0200

    .

--- src/main/resources/ui/tutorial/index.js
+++ src/main/resources/ui/tutorial/index.js
@@ -6,13 +6,16 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'))
 app.use(express.json());
 
+//frontend urls
 const url_frontend_adduser = '/users';
-const url_backend_adduser = 'http://localhost:8080/users';
+const url_frontend_getusers = '/users';
 
 app.post(url_frontend_adduser, (request, response) => {
+
+    const url_backend_adduser = 'http://localhost:8080/users';
+
     console.log('request: post, url: ' + url_frontend_adduser);
     console.log(request.body);
-    response.json(request.body);
 
     //request to backend
     const options = {
@@ -25,22 +28,31 @@ app.post(url_frontend_adduser, (request, response) => {
 
     fetch(url_backend_adduser, options);
 
+    //response
+    response.json(request.body);
 });
 
-const url_frontend_getusers = '/users';
-const url_backend_getusers = 'http://localhost:8080/users';
-
 app.get(url_frontend_getusers, (request, response) => {
+
+    const url_backend_getusers = 'http://localhost:8080/users';
+
     console.log('request: get, url: ' + url_frontend_getusers);
     console.log(request.body);
-    response.json(request.body);
+
 
     //request to backend
-    fetch(url_backend_adduser);
-    async function getUserswebserver() {
+    async function getUsers() {
 
         const response = await fetch(url_backend_getusers);
         const data = await response.json();
+        console.log(data);
 
     }
+
+    const promise = getUsers();  
+
+    console.log(promise);
+
+    //response
+    response.json(promise.body);
 });
